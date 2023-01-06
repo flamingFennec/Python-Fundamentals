74683873fcef86b41f1c909b81c4b49f36fbf173
flamingFennec <kattycakey5790@gmail.com>
2023-01-06 11:36:36 -0500

    e

---
+++ functions/functions_example.py
@@ -0,0 +1,7 @@
+def my_function():
+    print("I can use a function that has a piece of code to use in other parts of my code when need be.")
+    print("You can make variables in functions.")
+    print("def stands for define, next is the name of the function.")
+
+# To call a function:
+my_function()
---
+++ loops/while_loopeseseses.py
@@ -0,0 +1,6 @@
+i = float(input("What is a number under 10?: "))
+while i < 10:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 10")
